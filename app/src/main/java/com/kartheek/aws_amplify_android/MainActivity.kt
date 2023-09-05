2f650c18de1fce01d0384bec9178868d5905fcf6
karthik1220 <karthikgithub1102@gmail.com>
2023-09-05 22:30:52 +0530

    initial commit

---
+++ app/src/main/java/com/kartheek/aws_amplify_android/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.kartheek.aws_amplify_android
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.kartheek.aws_amplify_android.ui.theme.AWSAmplifyAndroidTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
+
